a16b6c7eac8a8e1909855bc598fca08187747b90
Keritan <kls625@msstate.edu>
2018-01-20 17:40:24 -0600

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  const colorInput = $('input[type=color]');
+  const sizeInput = $('input[type=submit]');
+  let height, width;
+  let values = {};
+  let table;
+
+  // When size is submitted by the user, call makeGrid()
+  $('#sizePicker').on('submit', makeGrid);
+
+  /*
+  * @description Makes the table grid. Clears the table on each submission.
+  * @param {object} event - Information about the event.
+  */
+  function makeGrid(event) {
+    event.preventDefault();
+    let row;
+    table = document.getElementById('pixel_canvas');
+
+    $.each($('#sizePicker').serializeArray(), function(i, field) {
+      values[field.name] = field.value;
+    });
+
+    if (table !== null && table.rows.length > 0) {
+      for (var row = 0; row < table.rows.length; i++) {
+        table.deleteRow(row);
+      }
+    }
+
+    height = +values.height;
+    width = +values.width;
+
+    if (table !== null) {
+      for (var i = 0; i < height; i++) {
+        row = table.insertRow(i);
+        for (var j = 0; j < width; j++) {
+          addClickListener(row.insertCell(j));
+        }
+      }
+    }
+  }
+
+  /*
+  * @description Adds a click event listener to the cell which changes its background color.
+  * @param {object} cell - The table cell.
+  */
+  function addClickListener(cell) {
+    $(cell).on('click', function() {
+      $(cell).css('background-color', colorInput.val());
+    });
+  }
+});
